099fdd34de8db12cf598b56c4a278f1c50c15371
DavidKizinger <karapetyand91@gmail.com>
2020-08-10 20:17:24 +0400

    last base form updated

--- src/components/Issues/issues.jsx
+++ src/components/Issues/issues.jsx
@@ -1,111 +1,52 @@
-import React, { useState } from "react";
+import React from "react";
 import "./issues.scss";
-import Issue from "./issue";
+import { Link } from "react-router-dom";
 
-const Issues = () => {
-  const [issues, setIssues] = useState([
-    {
-      id: 0,
-      text: "Hub Login command to initialise authentication",
-      isOpen: true,
-      number: "#8672",
-      badges: [
-        {
-          text: "issue: bug report",
-          color: "#EE5A24",
-        },
-        {
-          text: "issue: proposal",
-          color: "#ffd32a",
-        },
-      ],
-    },
-    {
-      id: 1,
-      text: "Use memo: true flag with SVGR",
-      isOpen: false,
-      number: "#86732",
-      badges: [
-        {
-          text: "needs triage",
-          color: "#EA2027",
-        },
-      ],
-    },
-    {
-      id: 2,
-      text: "Error during React project Run using npm start",
-      isOpen: true,
-      number: "#8672",
-      badges: [
-        {
-          text: "issue: bug report",
-          color: "#EE5A24",
-        },
-        {
-          text: "issue: proposal",
-          color: "#ffd32a",
-        },
-      ],
-    },
-    {
-      id: 3,
-      text: "Node packages do not update",
-      isOpen: true,
-      number: "#8672",
-      badges: [
-        {
-          text: "issue: bug report",
-          color: "#EE5A24",
-        },
-        {
-          text: "issue: proposal",
-          color: "#ffd32a",
-        },
-      ],
-    },
-    {
-      id: 4,
-      text: "Issue Related to React",
-      isOpen: false,
-      number: "#8672",
-      badges: [
-        {
-          text: "needs triage",
-          color: "#EA2027",
-        },
-      ],
-    },
-    {
-      id: 5,
-      text: "After create-react-app, `npm start` not work",
-      isOpen: true,
-      number: "#8672",
-      badges: [
-        {
-          text: "issue: bug report",
-          color: "#EE5A24",
-        },
-        {
-          text: "issue: proposal",
-          color: "#ffd32a",
-        },
-      ],
-    },
-  ]);
-
+const Issues = (props) => {
   return (
     <div className="issues">
-      {issues.map((issue) => {
+      {props.issues.map((issue) => {
         return (
-          <Issue
-            key={issue.id}
-            id={issue.id}
-            text={issue.text}
-            isOpen={issue.isOpen}
-            number={issue.number}
-            badges={issue.badges}
-          />
+          <div className="issue" key={issue.id}>
+            <Link
+              to={{
+                pathname: "/issue/${issue.id}",
+                details: {
+                  issue,
+                },
+              }}
+            />
+            <div className="header d-inline">
+              <span className="status">
+                {issue.state === "open" ? (
+                  <i class="fa fa-exclamation-circle"></i>
+                ) : (
+                  <i class="fa fa-check-circle" aria-hidden="true"></i>
+                )}
+              </span>
+              <h5 className="title d-inline">{issue.title}</h5>
+            </div>
+            <Link />
+            <div className="label d-inline">
+              {issue.labels.length
+                ? issue.labels.map((label) => {
+                    return (
+                      <span
+                        key={label.id}
+                        style={{ background: `#${label.color}` }}
+                      >
+                        {label.name}
+                      </span>
+                    );
+                  })
+                : null}
+            </div>
+            <div className="infoTicket">
+              <span className="id">#${issue.id}</span>
+              <span className="createAt">opened {issue.created_at}</span>
+              <span className="by">by {issue.user.login}</span>
+            </div>
+          </div>
         );
       })}
     </div>
