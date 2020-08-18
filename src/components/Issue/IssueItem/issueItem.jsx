a3a4bc119314e24afe8dacd35644023d9584bfc8
DavidKizinger <karapetyand91@gmail.com>
2020-08-19 00:49:08 +0400

    fixed all assignments

--- src/components/Issue/IssueItem/issueItem.jsx
+++ src/components/Issue/IssueItem/issueItem.jsx
@@ -3,6 +3,7 @@ import "../IssueStyle/issueStyle.scss";
 import { NavLink } from "react-router-dom";
 import LabelsType from "../../Label/LabelItem/LabelItem";
 import PropTypes from "prop-types";
+import Icon from "../../Icon/Icon";
 
 const Issue = ({ issue }) => {
   return (
@@ -10,24 +11,7 @@ const Issue = ({ issue }) => {
       <div className="issue">
         <div className="header">
           <span className="isOpen">
-            {issue.isOpen ? (
-              <svg
-                width="1em"
-                height="1em"
-                viewBox="0 0 16 16"
-                className="bi bi-exclamation-circle"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
-                />
-                <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z" />
-              </svg>
-            ) : (
-              <i className="fa fa-times-circle"></i>
-            )}
+            {issue.isOpen ? <Icon /> : <i className="fa fa-times-circle"></i>}
           </span>
           <h4 className="title">{issue.text}</h4>
           <span className="badges">
